0ed0f9165bd961a8345cf181dadba9d6dc04db63
pletcher <charles.pletcher@gmail.com>
2016-05-02 20:50:54 -0400

    Re-update spec
    My bad, @SophieDeBenedetto

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -3,7 +3,8 @@ require 'rails_helper'
 RSpec.describe Category, type: :model do
   describe 'relations' do
     it 'has many posts' do
-      expect(Category.new).to respond_to(:posts)
+      category = Category.create(name: "cats")
+      expect(category).to respond_to(:posts)
     end
   end
 end
