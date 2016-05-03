11a2f3280b30eb3c2f2173dfd52557138231b80f
pletcher <charles.pletcher@gmail.com>
2016-05-02 20:34:46 -0400

    Fix capitalization in Category spec
    Fixes #17

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -3,8 +3,7 @@ require 'rails_helper'
 RSpec.describe Category, type: :model do
   describe 'relations' do
     it 'has many posts' do
-      category = Category.create(name: "cats")
-      expect(category.new).to respond_to(:posts)
+      expect(Category.new).to respond_to(:posts)
     end
   end
 end
