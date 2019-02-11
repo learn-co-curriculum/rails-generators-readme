70aec4dc5a37e7cf914ddb9f2e714f2179475b99
maxwellbenton <maxwellbenton@gmail.com>
2019-02-11 15:05:55 -0500

    removes schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151230232412) do
+ActiveRecord::Schema.define(version: 20160113163910) do
+
   create_table "categories", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
@@ -23,5 +24,7 @@ ActiveRecord::Schema.define(version: 20151230232412) do
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "category_id"
   end
+
 end
