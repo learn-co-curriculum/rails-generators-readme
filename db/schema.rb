be88e7bcae33d99ca0941019dcfa051c3e4e81d7
drewsview34 <drewsview34@outlook.com>
2021-07-21 10:10:28 -0700

    finished lab

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210721163947) do
+ActiveRecord::Schema.define(version: 20210721170349) do
+
+  create_table "accounts", force: :cascade do |t|
+    t.string   "name"
+    t.string   "payment_status"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+  end
 
   create_table "authors", force: :cascade do |t|
     t.string   "name"
